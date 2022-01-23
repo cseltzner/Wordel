f10b56541026d591a95ead399dedfdcb60a60118
cseltz <chase.seltz21@gmail.com>
2022-01-22 20:09:01 -0800

    First commit, created basic layout.

---
+++ app/src/main/java/com/hfad/wordel/GameFragment.kt
@@ -0,0 +1,19 @@
+package com.hfad.wordel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class GameFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_game, container, false)
+
+
+        return view
+    }
+}
